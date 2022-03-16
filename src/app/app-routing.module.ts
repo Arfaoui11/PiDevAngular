6c970e6a189608569915866df668c2db5c4fc97d
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2022-03-16 14:02:40 +0100

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,56 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ListFomateurComponent} from "./list-fomateur/list-fomateur.component";
+import {FormationComponent} from "./formation/formation.component";
+import {CalendarComponent} from "./calendar/calendar.component";
+import {ListeFormationComponent} from "./liste-formation/liste-formation.component";
+import {QuizComponent} from "./quiz/quiz.component";
+import {CoursesFormComponent} from "./courses-form/courses-form.component";
+import {HomeComponent} from "./home/home.component";
+import {LayoutComponent} from "./layout/layout.component";
+import {AddFomateurComponent} from "./add-fomateur/add-fomateur.component";
+import {WelcomeComponent} from "./welcome/welcome.component";
 
-const routes: Routes = [];
+const routes: Routes =
+  /*[
+  {path : 'formateur' ,component: ListFomateurComponent},
+  {path: 'addFormation',component:FormationComponent},
+  {path: 'listFormateur',component:ListFomateurComponent},
+  {path: 'calendar',component:CalendarComponent},
+  {path: 'listFormation',component:ListeFormationComponent},
+  {path: 'quiz',component:QuizComponent},
+  {path: 'Courses',component:CoursesFormComponent},
+
+
+];*/
+   [ { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'Formation-management',
+        children: [
+
+          { path: 'addFormateur', component: AddFomateurComponent },
+          { path: 'formateur', component: ListFomateurComponent },
+          { path: 'addFormation', component: FormationComponent },
+          { path: 'listFormateur', component: ListFomateurComponent } ,
+          { path: 'calendar', component: CalendarComponent },
+          { path: 'listFormation', component: ListeFormationComponent },
+          { path: 'quiz', component: QuizComponent },
+          { path: 'quizWelcome', component: WelcomeComponent },
+          { path: 'Courses', component: CoursesFormComponent },
+
+        ]
+      },
+  ]
+  }
+];
+
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,89 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {GoogleChartsModule} from "angular-google-charts";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgxPaginationModule} from "ngx-pagination";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {CalendarModule, DateAdapter} from "angular-calendar";
+import {adapterFactory} from "angular-calendar/date-adapters/moment";
+import {RecurrenceEditorAllModule, ScheduleAllModule,DayService, WeekService, WorkWeekService, MonthService, AgendaService, TimelineViewsService,
+  TimelineMonthService, ResizeService, DragAndDropService, EventSettingsModel, ActionEventArgs,
+  ScheduleComponent, CellClickEventArgs, TimeScaleModel, GroupModel,
+  PopupOpenEventArgs, EJ2Instance, getWeekFirstDate, addDays, NavigatingEventArgs, WorkHoursModel} from "@syncfusion/ej2-angular-schedule";
+import { CalendarComponent } from './calendar/calendar.component';
+import {ListFomateurComponent} from "./list-fomateur/list-fomateur.component";
+import {FormationComponent} from "./formation/formation.component";
+import {AddFomateurComponent} from "./add-fomateur/add-fomateur.component";
+import { ListeFormationComponent } from './liste-formation/liste-formation.component';
+import {DayPilotModule} from "daypilot-pro-angular";
+
+
+
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import {TreeViewModule} from "@syncfusion/ej2-angular-navigations";
+import { QuizComponent } from './quiz/quiz.component';
+import { ChangeBgDirective } from './change-bg.directive';
+import {NgxQRCodeModule} from "ngx-qrcode2";
+import { CoursesFormComponent } from './courses-form/courses-form.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SidbarComponent } from './sidbar/sidbar.component';
+import {WelcomeComponent} from "./welcome/welcome.component";
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  interactionPlugin
+]);
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    ListFomateurComponent,
+    AddFomateurComponent,
+    FormationComponent,
+    ListeFormationComponent,
+    QuizComponent,
+    ChangeBgDirective,
+    CoursesFormComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    LayoutComponent,
+    SidbarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    NgxPaginationModule,
+    NgxQRCodeModule,
+    GoogleChartsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    TreeViewModule,
+    NgbModule,
+    FullCalendarModule,
+    DayPilotModule,
+    ScheduleAllModule,RecurrenceEditorAllModule,
   ],
-  providers: [],
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, TimelineViewsService,
+    TimelineMonthService, ResizeService, DragAndDropService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
